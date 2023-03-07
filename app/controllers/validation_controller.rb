460dc26a9c8da280332348fdeb85df94ffdea421
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-03-07 14:33:09 -0500

    added create controller

--- app/controllers/validation_controller.rb
+++ app/controllers/validation_controller.rb
@@ -1,2 +1,18 @@
 class ValidationController < ApplicationController
+  
+    def create
+
+      validation = Validation.new(
+        first_name: params[:first_name],
+        last_name: params[:last_name],
+        email: params[:email],
+        phone_number: params[:phone_number]
+      )
+      
+      if validation.save
+        render :json => @validations.as_json
+      else
+        render :json => @validations.errors.full_messages
+      :status => unprocessable_entity
+      end
 end
