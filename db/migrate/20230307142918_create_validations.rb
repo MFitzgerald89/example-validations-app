b9e4a742f12afd06fafb494d01ec9f5227427c67
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-03-07 12:34:50 -0500

    initial commit

---
+++ db/migrate/20230307142918_create_validations.rb
@@ -0,0 +1,12 @@
+class CreateValidations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :validations do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
